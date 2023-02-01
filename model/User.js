dca9ce9374cc7051217e07e8c6f3b16bfd91df65
SaboyaDev <saboyadev@gmail.com>
2023-02-01 00:50:40 -0500

    fixed the import of a module from mongoose

--- model/User.js
+++ model/User.js
@@ -1,4 +1,4 @@
-const { Schema } = require('mongoose')
+const { Schema, model } = require('mongoose')
 
 const userSchema = new Schema({
 	email: {
@@ -13,4 +13,4 @@ const userSchema = new Schema({
 	},
 })
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = model('User', userSchema)
